681ab887a8a38324edb479c6ff078e5312c785a7
Valeria <Koroleva.Valeria.97@yandex.ru>
2023-10-25 13:41:18 +0300

    запросы

---
+++ src/api/base-api.ts
@@ -0,0 +1,98 @@
+const enum Method {
+    GET = 'GET',
+    POST = 'POST',
+    PUT = 'PUT',
+    DELETE = 'DELETE',
+}
+// const jsonHeader = { 'Content-Type': 'application/json; charset=utf-8' };
+
+type RequestData = any;
+
+export class BaseApi {
+
+    private getFetchOptions = (method: Method, data?: RequestData) => ({
+        method,
+        // headers: jsonHeader,
+        body: JSON.stringify(data),
+    });
+
+    private checkResponse(response: Response) {
+        if (response.status !== 200) {
+            response.text().then((text: string) => {
+                throw new Error(text);
+            });
+        }
+        return response;
+    }
+
+    public async post({url, data }: {url: string, data?: RequestData }) {
+        console.log(data)
+        try {
+             await fetch(
+                `${url}`,
+                {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json;charset=utf-8'
+                    },
+                    body: JSON.stringify(data)
+                }
+            ).catch((error) => {
+               console.log(error);
+            });
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    public async get(url: string) {
+        try {
+            const response = await fetch(
+                `${url}`,
+                this.getFetchOptions(Method.GET),
+            ).catch((error) => {
+                throw new Error(error);
+            });
+            return await this.checkResponse(response).json();
+        } catch (error) {
+            if (error instanceof Error) {
+                throw new TypeError(`API Error: ${error.message}`);
+            }
+            throw new Error('Unknown API error');
+        }
+    }
+
+    // public async put({ data }: { data: RequestData }) {
+    //     try {
+    //         const response = await fetch(
+    //             `${this._baseUrl}`,
+    //             this.getFetchOptions(Method.PUT, data),
+    //         ).catch((error) => {
+    //             throw new Error(error);
+    //         });
+    //         return await this.checkResponse(response).json();
+    //     } catch (error) {
+    //         if (error instanceof Error) {
+    //             throw new TypeError(`API Error: ${error.message}`);
+    //         }
+    //         throw new Error('Unknown API error');
+    //     }
+    // }
+    //
+    // public async delete({ data }: { data: RequestData }) {
+    //     try {
+    //         const response = await fetch(
+    //             `${this._baseUrl}`,
+    //             this.getFetchOptions(Method.DELETE, data),
+    //         ).catch((error) => {
+    //             throw new Error(error);
+    //         });
+    //         return await this.checkResponse(response).json();
+    //     } catch (error) {
+    //         if (error instanceof Error) {
+    //             throw new TypeError(`API Error: ${error.message}`);
+    //         }
+    //         throw new Error('Unknown API error');
+    //     }
+    // }
+}
---
+++ src/controllers/order-controllers.ts
@@ -0,0 +1,27 @@
+import OrderApi from '../api/order-api'
+import {genOrderID} from "../utils/helpers";
+class OrderAPI {
+    OrderApi: OrderApi;
+    constructor() {
+        this.OrderApi = new OrderApi;
+    }
+    async makeOrder (dataProduct: any) {
+        const url = "/api/makeOrder";
+        const type = "purchase";
+        const data: any = {};
+
+        dataProduct.orderId = genOrderID()
+        data.type = type;
+        data.payload = dataProduct;
+        data.timestamp = Date.now()
+
+        try {
+            await this.OrderApi.makeOrder(url, data)
+        }
+        catch (error) {
+            console.log(error);
+        }
+    }
+
+}
+export default new OrderAPI();
